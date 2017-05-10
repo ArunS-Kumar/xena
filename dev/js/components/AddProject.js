72d2d1a9a0a919bb1430242118e4c8bcd71d1651
Arun Kumar <arunsak91@gmail.com>
2017-05-10 16:38:14 +0530

    complete route without#

---
+++ dev/js/components/AddProject.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class AddProject extends React.Component {
+
+	render() {
+		return (
+				<div className="container">
+					<h1>Project from</h1>
+				</div>
+			);
+	}
+}
+
+
--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { HashRouter, Route, Link } from 'react-router-dom';
-
-import {HeaderMenu} from './Header';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import {TimeLine} from './Timeline';
+import {AddProject} from './AddProject';
 
 require('../../scss/style.scss');
 
@@ -11,12 +10,37 @@ export default class App extends React.Component {
 	render() {
 		return (
 				<div>
-					<HeaderMenu />
-					<HashRouter>
+					<Router >
 						<div>
+							<nav role="navigation" className="navbar navbar-default">
+							    <div className="container">
+							        <div className="navbar-header">
+							            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle">
+							                <span className="sr-only">Toggle navigation</span>
+							                <span className="icon-bar"></span>
+							                <span className="icon-bar"></span>
+							                <span className="icon-bar"></span>
+							            </button>
+							            <Link to='/' className="navbar-brand"> XENA PROJECTS </Link>
+							        </div>
+							        <div id="navbarCollapse" className="collapse navbar-collapse">
+							            <ul className="nav navbar-nav">
+							                <li><Link to='/timeline'> Timeline </Link></li>
+							                <li><Link to='/addproject'> Add Project </Link></li>
+							                <li><Link to='/'>Messages </Link></li>
+							            </ul>
+							            <ul className="nav navbar-nav navbar-right">
+							                <li><a href="#">Login</a></li>
+							            </ul>
+							            
+							        </div>
+							    </div>
+							</nav>
+							<Route exact path = "/" component = {TimeLine} />
 						    <Route path = "/timeline" component = {TimeLine} />
-			            </div>
-					</HashRouter>
+						    <Route path = "/addproject" component = {AddProject} />
+					    </div>			
+					</Router>
 				</div> 
 			);
 	}
